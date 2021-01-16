1d820b2a55db7370c092f38a14b07e9857fc425a
Alamgirhossann <alamgirhossann@gmail.com>
2021-01-16 12:48:38 +0600

    port update

--- index.js
+++ index.js
@@ -26,7 +26,6 @@ MongoClient.connect(uri, function(err, client) {
   })
 
   app.get('/registeredUser',(req, res)=>{
-    const verifyEmail = req.headers.authorization;
     console.log(verifyEmail);
     userCollection.find({email: req.query.email})
     .toArray((err, document)=>{
@@ -69,65 +68,6 @@ MongoClient.connect(uri, function(err, client) {
 
   console.log('database connected');
 });
-
-
-
-// const MongoClient = require('mongodb').MongoClient;
-// const uri = "mongodb+srv://volunteer:volunteerNetwork50@cluster0.s5oej.mongodb.net/dbVolunteer?retryWrites=true&w=majority";
-// const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-// client.connect(err => {
-//   const registerCollection = client.db("dbVolunteer").collection("regInfo");
-//   const userCollection = client.db("dbVolunteer").collection("userInfo");
-
-//   app.get('/volunteerEvent',(req, res)=>{
-//     registerCollection.find({})
-//     .toArray((err, document)=>{
-//       res.send(document)
-//     })
-//   })
-
-//   // app.get('/registeredUser',(req, res)=>{
-//   //   userCollection.find({email: req.query.email})
-//   //   .toArray((err, document)=>{
-//   //     res.send(document)
-//   //   })
-//   // })
-
-//   // app.post('/volunteerEvents',(req, res)=>{
-//   //   const event = req.body;
-//   //   registerCollection.insertOne(event)
-//   //   .then(result => {
-//   //     console.log(result);
-//   //   })
-//   // })
-
-//   app.post('/registration',(req, res)=>{
-//     const regForm = req.body;
-//     console.log(regForm);
-//     userCollection.insertOne(regForm)
-//     .then(result => {
-//       console.log(result);
-//     })
-//   })
-
-//   app.get('/registeredEvent', (req, res)=>{
-//     userCollection.find({})
-//     .toArray((err, document)=>{
-//       res.send(document)
-//     })
-//   })
-
-//   app.delete('/delete/:id', (req, res)=>{
-//     console.log(req.params.id)
-//     userCollection.deleteOne({_id: ObjectId(req.params.id)})
-//     .then(result => {
-//       console.log(result);
-//       res.send(result.deletedCount > 0)
-//     })
-//   })
-
-
-// });
 
 
 app.get('/', (req, res) => {
